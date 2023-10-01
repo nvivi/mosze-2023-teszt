a2524b01a1f06cff366fef281eb94987d72a7e6c
nvivi <73818913+nvivi@users.noreply.github.com>
2023-10-01 15:54:31 +0200

    Add files via upload
    kikommentelese a talalt hibaknak

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Rosszul van írva a N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //"" kéne, hogy legyen és nincs ;
+    for (int i = 0;) //Hiányzik a feltétel, az i növelése
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //Hiányzik a feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Hiányzik a ; és a konrkét érték
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl; 
+    int atlag; //Nem adunk neki értéket
+    for (int i = 0; i < N_ELEMENTS, i++) 
     {
-        atlag += b[i]
+        atlag += b[i] //Nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
